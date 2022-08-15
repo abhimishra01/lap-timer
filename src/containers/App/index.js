ad177b2da2e01c59b4a768c9ea275ac5ce11390e
abhishek mishra <abhisk.mhra@gmail.com>
2022-08-15 13:40:31 +0530

    wip: stopwatch logic, onStart button

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,7 +1,29 @@
+import { useState } from "react";
+
+import "./styles.css";
+
 function App() {
+  const [seconds, setSeconds] = useState(0);
+
+  function onStart() {
+    setInterval(() => {
+      setSeconds(seconds + 1);
+    }, 1000);
+  }
+
   return (
     <div className="App">
-      <h2>Lap Timer</h2>
+      <h1>Lap Timer</h1>
+      <div className="App-timer">
+        <h2>{seconds}</h2>
+      </div>
+      <div className="App-action-btns">
+        <button id="reset">Reset</button>
+        <button onClick={onStart} id="start">
+          Start
+        </button>
+        <button id="stop">Stop</button>
+      </div>
     </div>
   );
 }
